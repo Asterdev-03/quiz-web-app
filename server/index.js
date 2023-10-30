9353c4d2f3017e4d3cbfdcc584d5a5e9530219a5
aswin <aswinkarat@gmail.com>
2023-10-31 00:54:48 +0530

    deploy server in render

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://quiz-web-app-cyan.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
